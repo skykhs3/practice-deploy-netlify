1550f3d85d8533f1855f4853cfa1e0461c362a3e
skykhs3 <skykhs3@naver.com>
2025-05-03 04:30:33 +0900

    feat: temporary

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import Script from 'next/script'
 
 export const metadata = {
     title: {
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
             <body className="antialiased text-white bg-blue-900">
+                <Script src="/redirect.js" />
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
---
+++ public/redirect.js
@@ -0,0 +1,3 @@
+window.location.replace(
+    'https://skykhs3.github.io/c-v/#KAIST%20(%20Korea%20Advanced%20Institute%20of%20Science%20and%20Technology%20)'
+);
